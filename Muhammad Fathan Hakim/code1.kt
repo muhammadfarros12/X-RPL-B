05e56175782b6b7cd4d227e809797fc33a2275a0
Fathkim <88641596+Fathkim@users.noreply.github.com>
2021-10-04 15:03:20 +0700

    Hactoberfest1

---
+++ Muhammad Fathan Hakim/code1.kt
@@ -0,0 +1,12 @@
+fun main(){
+   var fathan = 80
+
+   if(fathan < 76){
+       println("anda pintar")
+   }else if (fathan > 88){println("anda titisan einsten")
+  
+  }else if (fathan > 79){println("anda cerdas")
+   
+   }else if (fathan < 50){println("anda harus lebih banyak belajar")
+   }
+}
