d1fa42fb67904f4d39d5d921913c1bf04002fe25
Fathkim <88641596+Fathkim@users.noreply.github.com>
2021-10-05 07:38:16 +0700

    Hacktoberfest2

---
+++ Muhammad Fathan Hakim/kotlin.kt
@@ -0,0 +1,15 @@
+// mapKeys
+
+fun main() {
+   val nama = mapOf(
+       "Fathan" to "Ganteng",
+       "Fadi" to "Normal",
+       "Ka Farros" to "1 JT IQ"
+   )
+
+   val mapKeys = nama.keys
+   val mapValue = nama.values
+
+   println(mapKeys)
+   println(mapValue)
+}
